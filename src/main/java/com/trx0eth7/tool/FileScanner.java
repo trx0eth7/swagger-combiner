ef62fc9ac187b9df17652eb410990c1e4efd4735
trx <bahdepa2@gmail.com>
2019-01-14 00:52:25 +0400

    General facades for combiner API

---
+++ src/main/java/com/trx0eth7/tool/FileScanner.java
@@ -0,0 +1,7 @@
+package com.trx0eth7.tool;
+
+import java.util.List;
+
+public interface FileScanner {
+    List<String> scan(String pathDir);
+}
---
+++ src/main/java/com/trx0eth7/tool/OpenAPIV3Сombiner.java
@@ -0,0 +1,6 @@
+package com.trx0eth7.tool;
+
+public interface OpenAPIV3Сombiner {
+
+    String combine(String scanPathDir);
+}
